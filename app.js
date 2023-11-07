e3003a73f5e697f9bb02192b0a2f4d86c811562e
EBN10 <basilemir@gmail.com>
2023-11-07 12:10:31 -0300

    Trabajo terminado

---
+++ app.js
@@ -0,0 +1,28 @@
+const express=require('express'); // Requiero el modulo express
+const path= require('path'); // Guardo en la variable la funcion express
+const app=express() // Requiero el modulo nativo path
+const publicPath= path.resolve(__dirname,'./public');
+
+app.use(express.static(publicPath));
+
+
+//Creando Rutas
+
+app.get('/',(req,res)=>{
+    res.sendFile(path.join(__dirname,'/views/home.html'))
+});
+app.get("/public/css/style.css", (req, res) => {
+    res.sendFile(path.join(__dirname, "/public/css/style.css"));
+})
+app.get('/register',(req,res)=>{
+    res.sendFile(path.join(__dirname,'/views/register.html'));
+})
+app.get("/login", (req, res) => {
+  res.sendFile(path.join(__dirname, "/views/login.html"));
+});
+
+//Levantando el Servidor
+
+app.listen(3001,() =>{
+    console.log('Servidor corriendo en el puerto 3001');
+})
